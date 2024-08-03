fe9694479a5365ad8d1062de1e8f9d612ef3aae6
Kalu <bgd.krb.kalu@gmail.com>
2024-08-03 20:52:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Pozdrav iz konzole!")
+//window.alert('Pozdrav iz Pop Up-a')
+console.error('duvaj ga')
+console.warn('bakar')
+console.log(5+5)
+console.log(5 === 5)
+console.log(5 === 6)
